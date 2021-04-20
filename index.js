e144665a34d2b984b564c10ace231644393804b0
levanchuong <trumle2k1@gmail.com>
2021-04-20 07:46:20 +0700

    commit number 6

--- index.js
+++ index.js
@@ -141,5 +141,5 @@ app.get('/admin',admin)
 
 app.get('/news/timkiem', timkiem.search);
 app.get('/page/:idtintuc', page.loadpage)
-app.listen(process.env.PORT, function(){
+app.listen(3000, function(){
 })
