f2e120ad6fc8396353069de4c298a5da8647dde5
levanchuong <trumle2k1@gmail.com>
2021-05-30 21:13:18 +0700

    commit number 15

--- connectDatabase/Connection.js
+++ connectDatabase/Connection.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var conn = mysql.createConnection({
-    host: 'sql6.freemysqlhosting.net',port: 3306,
-    user: 'sql6413081',
-    password: '1RmmzGQxYH',
-    database: 'sql6413081',
+    host: 'db4free.net',port: 3306,
+    user: 'homnaytuibuon2k1',
+    password: 'Chuong03022001',
+    database: 'homnaytuibuon',
     multipleStatements: true,
     connectionLimit: 100
 });
